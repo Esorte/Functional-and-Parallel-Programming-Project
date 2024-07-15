72f8c594bc045e39f50bebf3585e8a900218d7b7
Esorte <76818934+Esorte@users.noreply.github.com>
2024-07-15 22:09:39 +0700

    parral code format

--- Parallel/src/main/scala/Main.scala
+++ Parallel/src/main/scala/Main.scala
@@ -1,5 +1,25 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+package sequentialpar
 
-def msg = "I was compiled by Scala 3. :)"
+object Main extends App {
+  val initialDigits = Map(
+    (0, 1) -> 2, (0, 2) -> 3,
+    (3, 1) -> 4, (3, 2) -> 2
+  )
+  val constraints = List(
+    ((1, 1), (2, 1)),
+    ((3, 2), (3, 3))
+  )
+
+  val puzzle = new FutoshikiPuzzle(4, initialDigits, constraints)
+  println("Initial Puzzle:")
+  println(puzzle)
+
+  ParSolver.parSolve(puzzle) match {
+    case Some(solvedBoard) =>
+      println("Solved Puzzle:")
+      for (row <- solvedBoard) {
+        println(row.mkString(" "))
+      }
+    case None => println("No solution found.")
+  }
+}
